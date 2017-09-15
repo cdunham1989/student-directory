dfcfe3607a482751617e3a50019deea9b0a61010
Chris <cdunham1989@gmail.com>
2017-09-15 12:26:13 +0100

    Step 14 exercise 7

---
+++ Step14/exercise7.rb
@@ -0,0 +1,113 @@
+require 'csv'
+@students = []
+
+def input_students
+  puts "Please enter the name of the students"
+  puts "To finish, just hit return twice."
+  name =  STDIN.gets.chomp
+  while !name.empty? do
+    add_student(name)
+    puts "Now we have #{@students.count} students"
+    name = STDIN.gets.chomp
+  end
+  puts "Students added!"
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the Students"
+  puts "2. Show the Students"
+  puts "3. Save the list to file"
+  puts "4. Load the list from file"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1" then input_students
+  when "2" then show_students
+  when "3" then save_students
+  when "4" then load_students
+  when "9" then exit
+  else puts "I don't know what you meant, try again."
+  end
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print_student_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
+
+def save_students
+  puts "What would you like to call the file?"
+  filename = STDIN.gets.chomp.strip
+  CSV.open(filename, "w") do |csv|
+    @students.each { |student| csv << [student[:name], student[:cohort]]}
+  # File::open(filename, "w") do |f|
+  #   @students.each do |student|
+  #     student_data = [student[:name], student[:cohort]]
+  #     csv_line = student_data.join(",")
+  #     p csv_line
+  #     f.puts csv_line
+  #   end
+  # end
+  end
+  puts "List saved to #{filename}"
+end
+
+def load_students(filename = "students.csv")
+  puts "What file would you like to load?"
+  filename = STDIN.gets.chomp.strip
+  CSV.foreach(filename) do |line|
+    add_student(line[0])
+  # File::open(filename, "r") do |f|
+  #   f.readlines.each do |line|
+  #     name, cohort = line.chomp.split(',')
+  #       add_student(name)
+  #     end
+  #   end
+  end
+  puts "List loaded from students.csv"
+end
+
+def load_student_file
+  filename = ARGV.first
+  if filename.nil?
+    load_students
+  elsif File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry #{filename} doesn't exist."
+    exit
+  end
+end
+
+def add_student(name)
+  @students << {name: name, cohort: :november}
+end
+
+load_student_file
+interactive_menu
